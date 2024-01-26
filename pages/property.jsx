78ce83fa50bbb88e838059c1dc364d2a2c8c513e
Aman Deep <aman.dp121@gmail.com>
2024-01-26 13:45:01 +0530

    change1

--- pages/property.jsx
+++ pages/property.jsx
@@ -24,7 +24,7 @@ const property = () => {
 
       <div className='max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 py-8'>
         <div className='col-span-4'>
-          <p>Project</p>
+          <p>Projects</p>
           <h2>Overview</h2>
           <p>
             This app was built using React JS and is hosted on Firebase. Users
